cfea3c0501310874da271dc580c0b2cdb3cd3248
Samz0001 <sishere23@gmail.com>
2024-02-03 20:08:24 +0530

    Fixed Copy Function

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -31,10 +31,7 @@ export default function TextForm(props) {
   };
 
   const handleCopy = () => {
-    var text = document.getElementById("myBox");
-    text.select();
-    navigator.clipboard.writeText(text.value);
-    document.getSelection().removeAllRanges();
+    navigator.clipboard.writeText(text);
     props.showAlert("Copied Text to ClipBoard", "success");
   };
   const [text, setText] = useState("");
